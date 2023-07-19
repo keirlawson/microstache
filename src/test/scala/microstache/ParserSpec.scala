d41ffef279c074bda4c8d63dd63a7a0beb3f3fec
Keir Lawson <keirlawson@gmail.com>
2023-07-19 13:28:35 +0100

    Initial test

--- src/test/scala/microstache/ParserSpec.scala
+++ src/test/scala/microstache/ParserSpec.scala
@@ -1,7 +1,15 @@
 package microstache
 
+import Parser.parser
+import cats.syntax.all._
+import cats.parse.Parser.Error
+
 class HelloSpec extends munit.FunSuite {
-  test("say hello") {
-    assertEquals(Hello.greeting, "hello")
+  test("parses expression between two texts") {
+    assertEquals(parser.parse("foo{{bar}}baz"), ("", Ast.Template(List(
+      Ast.Text("foo"),
+      Ast.Expression("bar"),
+      Ast.Text("baz")
+    ))).asRight[Error])
   }
 }
