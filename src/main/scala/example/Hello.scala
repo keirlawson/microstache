5b9815fba3688f4109f18c4bce0cb92bfafce31f
Keir Lawson <keirlawson@gmail.com>
2023-07-19 13:18:59 +0100

    Parse expressions from broad text

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,25 +2,20 @@ package example
 
 import cats.effect.IOApp
 import cats.effect.IO
-import cats.parse.Parser
-import cats.parse.Rfc5234._
 
 object Ast {
-  case class Template(contents: List[Foo])
+  case class Template(contents: List[Term])
+  sealed trait Term
+  case class Text(value: String) extends Term
+  case class Expression(value: String) extends Term
 }
 
 object Hello extends IOApp.Simple {
-  val template = "foo{{bar}}baz"
+  val template = "foo{{bar}}baz{{qux}}"
   
-  val parser = {
-    val generalText = alpha.repAs0[String].filter(s => !s.contains("{{"))
-    val openExpression = Parser.string("{{")
-    val closeExpression = Parser.string("}}")
-    val expression = alpha.repAs[String].between(openExpression, closeExpression)
-    generalText ~ expression ~ generalText
-  }
+
   
-  val result = parser.parse(template)
+  val result = Parser.parser.parse(template)
 
   val run = IO.println(result)
 }
---
+++ src/main/scala/example/Parser.scala
@@ -0,0 +1,21 @@
+package example
+
+import cats.parse.{Parser => P}
+import cats.parse.Rfc5234._
+
+object Parser {
+      val parser = {
+    val generalText = alpha.repAs[String].filter(s => !s.contains("{{")).map(Ast.Text)
+    val openExpression = P.string("{{")
+    val closeExpression = P.string("}}")
+    val expression = alpha.repAs[String].between(openExpression, closeExpression).map(Ast.Expression)
+    (generalText.?.with1 ~ expression ~ generalText.?).rep0.map { lst =>
+      val combined = lst.flatMap { case ((before, exp), after) => 
+        List(before, Some(exp), after).flatten
+      }
+      Ast.Template(combined)
+    }
+    //.map(nel => nel.toList.flatMap(x => List[Ast.Term](x._1, x._2))).map(Ast.Template)
+    // generalText ~ expression ~ generalText
+  }
+}
